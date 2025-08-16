f615b0ac6a8849df91d1303d9754e59fb3a092a7
SofiaGerv <sgervasi@estudiantes.unsam.edu.ar>
2025-08-16 15:24:41 -0300

    Create

---
+++ TS0 Gervasi.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 13 19:11:47 2025
+
+@author: Sofía
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Defino variables
+A= 2  #Volts ; Energia=A^2/2
+dc = 0  #Volts
+f0 = 10  #Hz
+fase = 0 #Radianes
+N = 1000
+fs = 1000 #Hz
+## 1/fs=Ts 
+
+## A medida que aumento la frecuencia, disminuyo la cantidad de muestras por periodo
+## En nyquist va a haber 2N/fs muestras, como N=fs, vamos a tener 2 muestras por periodo
+
+#Defino la funcion
+def mi_funcion_sen(A, dc, f0, fase, N, fs):
+    tt= np.linspace(0,(N-1)*(1/fs),N)
+    xx = A* np.sin(2 * np.pi * f0 * tt + fase) + dc
+    return xx, tt
+
+xx, tt = mi_funcion_sen(A, dc, f0, fase, N, fs)
+
+potencia=(1/N)*np.sum(xx**2)
+
+# Grafico la señal
+plt.figure(1)
+plt.plot(tt, xx)
+
+## Grafico para f0=500 Hz (Nyquist)
+f0=500
+
+xx, tt = mi_funcion_sen(A, dc, f0, fase, N, fs)
+
+plt.figure(2)
+plt.plot(tt, xx,':o')
+
+## Como la fase es 0, los dos unicos puntos que deberia ver son los ceros del seno. Si cambio la fase, por ejemplo a pi/2, deberia ver los maximos 
+
+## Grafico para f0=999 Hz
+f0=999
+
+xx, tt = mi_funcion_sen(A, dc, f0, fase, N, fs)
+
+plt.figure(3)
+plt.plot(tt, xx)
+
+## Grafico para f0=1001 Hz
+f0=1001
+
+xx, tt = mi_funcion_sen(A, dc, f0, fase, N, fs)
+
+plt.figure(4)
+plt.plot(tt, xx)
+
+## Grafico para f0=2001 Hz
+f0=2001
+
+xx, tt = mi_funcion_sen(A, dc, f0, fase, N, fs)
+
+plt.figure(5)
+plt.plot(tt, xx)
+
+
+
